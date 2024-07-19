95593af4f26e3872e53c323bb59215a9e587a3a1
dunnwithpuns <dunnwithpuns@gmail.com>
2024-07-19 13:55:08 -0500

    finished window class and added main.py

--- window.py
+++ window.py
@@ -7,4 +7,18 @@ class Window:
         self.__root = Tk()
         self.__root.title = "Maze Solver"
         self.canvas = canvas(self.__root, bg="white")
+        self.canvas.pack()
+        self.is_running = False
 
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running == True:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
