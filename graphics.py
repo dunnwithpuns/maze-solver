49339b0b00e9d2f1d2756168cfc1df4cad520ce1
dunnwithpuns <dunnwithpuns@gmail.com>
2024-07-25 11:04:41 -0500

    created cell class

--- graphics.py
+++ graphics.py
@@ -19,11 +19,11 @@ class Window:
             self.redraw()
         print("window closed...")
  
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
     def close(self):
         self.is_running = False
-
-    def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
 
 class Point:
     def __init__(self, x=0, y=0):
@@ -39,3 +39,40 @@ class Line:
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
 
 
+class Cell:
+    def __init__(
+            self, 
+            window,     
+            left="True", right="True", top="True", bottom="True",
+            ):
+
+        self.has_left_wall = left
+        self.has_right_wall = right
+        self.has_top_wall = top
+        self.has_bottom_wall = bottom
+        self.__win = window
+       
+    def draw(self, top_left, bot_right):
+        self.__x1 = top_left.x
+        self.__y1 = top_left.y
+        self.__x2 = bot_right.x
+        self.__y2 = bot_right.y
+
+        if self.__x1 >= self.__x2 or self.__y1 >= self.__y2:
+            return ValueError("invalid cell size")
+        
+        if self.has_left_wall:
+            self.left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(self.left_wall)
+
+        if self.has_right_wall:
+            self.right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(self.right_wall)
+
+        if self.has_top_wall:
+            self.top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(self.top_wall)
+        
+        if self.has_bottom_wall:
+            self.bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(self.bottom_wall)
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
-    p1 = Point(0, 0)
-    p2 = Point(800, 600)
-    line1 = Line(p1, p2)
-    win.draw_line(line1, "red")
-    p3 = Point(0, 600)
-    p4 = Point(800, 0)
-    line2 = Line(p3, p4)
-    win.draw_line(line2, "green")
+
+    cell = Cell(win)    
+    p1 = Point(50, 50)
+    p2 = Point(55, 55)
+    p3 = Point(100, 50)
+    p4 = Point(150, 100)
+
+    cell.draw(p1, p2)
+    cell.draw(p3, p4)
+
     win.wait_for_close()
 
 if __name__ == "__main__":
