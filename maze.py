eea7576e1d84df7ce894bae65a9f6b843d820c29
dunnwithpuns <dunnwithpuns@gmail.com>
2024-07-26 11:31:05 -0500

    considering to use matrix/graph for the _draw_cells() method, have to go to work tho :(

--- maze.py
+++ maze.py
@@ -17,10 +17,27 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        
-        _create_cells(self)
-
-    def _create_cells(self):
         self._cells = []
-        # for col in self._num_cols:
-            
+        self._create_cells()
+
+    def _create_cells(self): 
+        
+        for i in self._num_cols:
+            col = []
+            for j in self._num_rows:
+                col.append(Cell(self._win))
+            self._num_cols.append(col)
+
+        for i in range(len(self._cells)):
+            for j in range(len(col)):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        
+        if i == 0 and j == 0:
+            x1 = self._x1
+            y1 = self._y1
+            x2 = x1 + self._cell_size_x
+            y2 = y1 + self._cell_size_y
+
+
