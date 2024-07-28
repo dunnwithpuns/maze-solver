a45e77490b1d6d7a704ca53cd65336e109d1a451
dunnwithpuns <dunnwithpuns@gmail.com>
2024-07-27 22:52:44 -0500

    completed first maze creation unit test

--- maze.py
+++ maze.py
@@ -18,16 +18,16 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        self._cells = []
         self._create_cells()
 
-    def _create_cells(self): 
-        
-        for i in self._num_cols:
+    def _create_cells(self):
+
+        self._cells = []
+        for i in range(self._num_cols):
             col = []
-            for j in self._num_rows:
+            for j in range(self._num_rows):
                 col.append(Cell(self._win))
-            self._num_cols.append(col)
+            self._cells.append(col)
 
         for i in range(len(self._cells)):
             for j in range(len(col)):
@@ -35,24 +35,22 @@ class Maze:
 
     def _draw_cell(self, i, j):
         
-        for i in range(len(self._cells)):
-            for j in range(i):
-                # calculate x1 based on current col index (i) and cell size 
-                x1 = self._x1
-                if i != 0: 
-                    x1 += i * self._cell_size_x   
-               
-                # calculate y1 based on current row index (i) and cell size 
-                y1 = self._y1
-                if j != 0:
-                    y1 += j * self._cell_size_y
-                
-                # calculate x2 and y2 using x1 and y1 and cell_size
-                x2 = x1 + self._cell_size_x
-                y2 = y1 + self._cell_size_y
+       # calculate x1 based on current col index (i) and cell size 
+        x1 = self._x1
+        if i != 0: 
+            x1 += i * self._cell_size_x   
+       
+        # calculate y1 based on current row index (i) and cell size 
+        y1 = self._y1
+        if j != 0:
+            y1 += j * self._cell_size_y
+        
+        # calculate x2 and y2 using x1 and y1 and cell_size
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
 
-                self._cells[i][j].draw(x1, y1, x2, y2)
-                self._animate()
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
 
     def _animate(self):
         self._win.redraw()
--- tests.py
+++ tests.py
@@ -1,14 +1,16 @@
 import unittest
+from graphics import Window
 from maze import Maze
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
+        win = Window(800, 600)
         num_cols = 12
         num_rows = 10
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
         self.assertEqual(
             len(m1._cells),
-            num_cols,
+            num_cols, 
         )
         self.assertEqual(
             len(m1._cells[0]),
